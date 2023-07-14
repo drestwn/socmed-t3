737dd64cdcecb33e26bcd60b76be2431d6b10095
drestwn <andremakmursetaiwan@gmail.com>
2023-07-14 21:04:49 +0700

    feat:update

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -6,22 +6,53 @@ import {
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
-  infiniteFeed:publicProcedure.input(
+  infiniteFeed: publicProcedure.input(
     z.object({
-      limit: z.number().optional(), 
-      cursor:z.object({ id:z.string(),createdAt:z.date()}).optional()
+      limit: z.number().optional(),
+      cursor: z.object({ id: z.string(), createdAt: z.date() }).optional()
     })
-    ).query(async({input:{limit=10,cursor},ctx})=>{
-      ctx.prisma.tweet.findMany({
-    take:limit+1,
-    cursor:cursor? {createdAt_id:cursor}:undefined,
-    orderBy:[{createdAt:"desc"},{id:"desc"}]
-  })
-}),
+  ).query(async ({ input: { limit = 10, cursor }, ctx }) => {
+    const currentUserId = ctx.session?.user.id
+    const data = await ctx.prisma.tweet.findMany({
+      take: limit + 1,
+      cursor: cursor ? { createdAt_id: cursor } : undefined,
+      orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+      select: {
+        id: true,
+        content: true,
+        createdAt: true,
+        _count: { select: { likes: true } },
+        likes: currentUserId == null ? false : { where: { userId: currentUserId } },
+        user: {
+          select: { name: true, id: true, image: true }
+        }
+      }
+    })
+    let nextCursor: typeof cursor | undefined
+    if (DataTransfer.length > limit) {
+      const nextItem = data.pop()
+      if (nextItem != null) {
+
+        nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt }
+      }
+    }
+    return {
+      tweets: data.map(tweet => {
+        return {
+          id: tweet.id,
+          content: tweet.content,
+          createdAt: tweet.createdAt,
+          likeCount: tweet._count.likes,
+          user: tweet.user,
+          likedBy: tweet.likes?.length > 0
+        }
+      }), nextCursor
+    }
+  }),
   create: protectedProcedure
     .input(z.object({ text: z.string() }))
     .mutation(async ({ input: { text }, ctx }) => {
-      const tweet=ctx.prisma.tweet.create({
+      const tweet = ctx.prisma.tweet.create({
         data: { content: text, userId: ctx.session.user.id },
       });
       return tweet
